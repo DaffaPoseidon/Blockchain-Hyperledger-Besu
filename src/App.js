190947c715a18da7b3ee4eee0ecfd02e1b737361
DaffaPoseidon <ndap.cahyo@gmail.com>
2024-08-02 19:06:27 +0700

    1.1 Update Images

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path='/:slug' element={<Detail/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,76 @@
+import imageProduct1 from './assets/images/emisi_karbon.jpeg'
+import imageProduct2 from './assets/images/emisi_karbon.jpeg'
+import imageProduct3 from './assets/images/emisi_karbon.jpeg'
+import imageProduct4 from './assets/images/emisi_karbon.jpeg'
+import imageProduct5 from './assets/images/emisi_karbon.jpeg'
+import imageProduct6 from './assets/images/emisi_karbon.jpeg'
+import imageProduct7 from './assets/images/emisi_karbon.jpeg'
+import imageProduct8 from './assets/images/emisi_karbon.jpeg'
+
+
+export const products = [
+    {
+        id: 1,
+        name: 'Emisi Karbon Perusahaan A',
+        price: 50,
+        image: imageProduct1,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-a'
+    },
+    {
+        id: 2,
+        name: 'Emisi Karbon Perusahaan B',
+        price: 100,
+        image: imageProduct2,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-b'
+    },
+    {
+        id: 3,
+        name: 'Emisi Karbon Perusahaan C',
+        price: 150,
+        image: imageProduct3,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-c'
+    },
+    {
+        id: 4,
+        name: 'Emisi Karbon Perusahaan D',
+        price: 200,
+        image: imageProduct4,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-d'
+    },
+    {
+        id: 5,
+        name: 'Emisi Karbon Perusahaan E',
+        price: 250,
+        image: imageProduct5,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-e'
+    },
+    {
+        id: 6,
+        name: 'Emisi Karbon Perusahaan F',
+        price: 300,
+        image: imageProduct6,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-f'
+    },
+    {
+        id: 7,
+        name: 'Emisi Karbon Perusahaan g',
+        price: 350,
+        image: imageProduct7,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-h'
+    },
+    {
+        id: 8,
+        name: 'Emisi Karbon Perusahaan I',
+        price: 400,
+        image: imageProduct8,
+        description: 'lorem ipsum',
+        slug: 'emisi-karbon-perusahaan-i'
+    },
+];
