17a7062b3c5c08b2f35677bb532cc533ed433601
DaffaPoseidon <ndap.cahyo@gmail.com>
2024-08-02 17:37:40 +0700

    Versi 1.0

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/layout';
+import Home from './pages/home';
+import Detail from './pages/detail';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
